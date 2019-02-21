ef7d14cf5ddafa87e1c1bded62d0da7954f2f2b1
natalia.v <natalia.v@gvocorp.com>
2019-02-21 18:34:27 +0200

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 
 $(document).ready(function () {
- 
-        
+
+
+    var pathname = window.location.pathname;
+    $('#navbarsconf a').each(function () {
+        var attr = $(this).attr('href');
+        var entry = pathname.indexOf(attr);
+        if (entry > 0) {
+            $(this).parents('li').addClass('active');
+        }
+    });
+
+
+    $('#imageModal').on('show.bs.modal', function (e) {
+        var image = $(e.relatedTarget).attr('data-src'),
+            title = $(e.relatedTarget).attr('data-title');
+        $('.img-modal').attr('src', image);
+        $('.modal-title').text(title);
+    });
+
+
 });
